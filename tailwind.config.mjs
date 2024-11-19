c47bdde9ec46af722f5791b53c790f99f537213d
John-Alz <99442406+John-Alz@users.noreply.github.com>
2024-11-18 20:40:16 -0500

    feat: hero

---
+++ tailwind.config.mjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {},
+		colors: {
+			'white': '#FFFFFF',
+			'black': '#000000',
+			'grayv1': '#C4C4C4',
+			'violet': '#763CAC',
+			'blackBg': '#1E1E1E',
+		},
+		// fontSize: {
+		// 	'lg': '20px',
+		// 	'xl': '24px',
+		// 	'9xl': '140px'
+		// }
+	},
+	plugins: [],
+}
