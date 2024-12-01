6bfbd7196a0a48c5cdde4e3c1ffaa49bafb79170
John-Alz <99442406+John-Alz@users.noreply.github.com>
2024-11-30 22:20:40 -0500

    feat: gsap animations

---
+++ src/components/marquee/MarqueeWrapper.tsx
@@ -0,0 +1,37 @@
+
+import Marquee from 'react-fast-marquee';
+import './marqueeWrapper.css'
+
+type Item = {
+  title: string
+  img: string
+}
+
+const items: Item[] = [
+  {title: "Diseño Web", img: "../../../public/imgs/react.svg"},
+  {title: "Desarrollo Front-End", img: "../../../public/imgs/react.svg"},
+  {title: "Desarrollo Back-End", img: "../../../public/imgs/react.svg"},
+  {title: "Responsive Design", img: "../../../public/imgs/react.svg"},
+]
+
+export default function MarqueeWrapper() {
+
+
+  return (
+    <div className=' text-white text-[160px] font-bold mt-10'>
+      <Marquee speed={70} >
+        {
+          items.map((item) => {
+            return (
+              <div className='flex gap-12 cursor-pointer'>
+                <h2 className='item-text'>{item.title}</h2>
+                <img className='translate-y-5' src={item.img} alt="icon-react" />
+                <div></div>
+              </div>
+            )
+          })
+        }
+      </Marquee>
+    </div>
+  );
+}
